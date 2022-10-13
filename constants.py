1868bbfd01865ee2af70877a00a2ef1c2f1900a6
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2022-10-13 21:39:27 +0200

    Some tests on scipy sparse

--- constants.py
+++ constants.py
@@ -6,3 +6,12 @@ ROOT_DIR = Path.cwd()
 INSTANCES_DIR = ROOT_DIR / "instances"
 BASE_INSTANCES_DIR = INSTANCES_DIR / "project_instances"
 OTHER_INSTANCES_DR = INSTANCES_DIR / "other_instances"
+# Instances files groups
+BASE_INSTANCES_FILES = [
+    "brock200_2.col",
+    "dsjc125.1.col",
+    "random-10.col",
+    "random-100.col",
+    "random-40.col",
+    "random-70.col",
+]
--- utils/instance_reading_utils.py
+++ utils/instance_reading_utils.py
@@ -1,6 +1,7 @@
 from typing import Tuple, List, TextIO
 
 import numpy as np
+from scipy.sparse import csr_array
 
 
 def read_instance_first_line(instance_file: TextIO) -> Tuple[int, int]:
@@ -33,7 +34,7 @@ def update_adjacency_matrix_with_edge(
     return
 
 
-def read_single_problem_from_file(
+def read_single_problem_from_file_as_adjacency(
     instance_file: TextIO,
 ) -> Tuple[int, int, np.ndarray]:
     """
@@ -54,21 +55,70 @@ def read_single_problem_from_file(
     return number_of_nodes, number_of_edges, adjacency_matrix
 
 
-def read_single_problem_from_path(
+def read_single_problem_from_path_as_adjacency(
     instance_path: str,
-) -> Tuple[str, int, int, int, List[int]]:
+) -> Tuple[int, int, np.ndarray]:
     """
     Used to read a file containing a unique problem from given path.
     """
     with open(instance_path, "r") as instance_file:
-        return read_single_problem_from_file(instance_file)
+        return read_single_problem_from_file_as_adjacency(instance_file)
+
+
+def read_single_problem_from_path_as_sparse(
+    instance_path: str,
+) -> Tuple[int, int, csr_array]:
+    """
+    Used to read a file containing a unique problem from given path and
+    return the parameters, with the graph being represented as a scipy
+    sparse matrix.
+    """
+    (
+        number_of_nodes,
+        number_of_edges,
+        adjacency_matrix,
+    ) = read_single_problem_from_path_as_adjacency(instance_path)
+    return number_of_nodes, number_of_edges, csr_array(adjacency_matrix)
 
 
 if __name__ == "__main__":
-    from constants import BASE_INSTANCES_DIR
+    from constants import BASE_INSTANCES_DIR, BASE_INSTANCES_FILES
 
     print(
-        read_single_problem_from_path(
-            instance_path=BASE_INSTANCES_DIR / "random-10.col"
+        read_single_problem_from_path_as_adjacency(
+            # instance_path=BASE_INSTANCES_DIR/ "brock200_2.col"
+            instance_path=BASE_INSTANCES_DIR
+            / "random-10.col"
         )
     )
+
+    """
+    from time import time
+
+    number_of_loads = 300
+
+    start_time = time()
+    for _ in range(number_of_loads):
+        read_single_problem_from_path_as_adjacency(
+            instance_path=BASE_INSTANCES_DIR / "brock200_2.col"
+        )
+    print(f"Mean time :{(time() - start_time)/number_of_loads}")
+    """
+
+    """
+    for file in BASE_INSTANCES_FILES:
+        _, _, array = read_single_problem_from_path_as_sparse(BASE_INSTANCES_DIR / file)
+        _, _, adjacency = read_single_problem_from_path_as_adjacency(
+            BASE_INSTANCES_DIR / file
+        )
+        from sys import getsizeof
+
+        print(
+            file,
+            getsizeof(array.indices)
+            + getsizeof(array.indptr)
+            + getsizeof(array.data)
+            + getsizeof(array),
+            getsizeof(adjacency),
+        )
+    """
