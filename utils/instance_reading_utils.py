f5a69da32755508d5ba616cf01718bf2d00a91e8
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2022-10-14 13:59:04 +0200

    Added sparse matrix

--- utils/instance_reading_utils.py
+++ utils/instance_reading_utils.py
@@ -40,8 +40,7 @@ def read_single_problem_from_file_as_adjacency(
     """
     Used to read a file containing a unique problem.
 
-    There is a question of how to store the graph. First let us use a simple
-    node-node adjacency matrix.
+    Stores the result as an adjacency matrix
     """
     number_of_nodes, number_of_edges = read_instance_first_line(instance_file)
     adjacency_matrix = initialize_adjacency_matrix(number_of_nodes)
@@ -65,7 +64,78 @@ def read_single_problem_from_path_as_adjacency(
         return read_single_problem_from_file_as_adjacency(instance_file)
 
 
+def initialize_sparse_array(
+    number_of_nodes, number_of_edges
+) -> Tuple[np.ndarray, list, list]:
+    """
+    Indptr will be of size n + 1 but we don't know the values so we only put zeroes
+    at first.
+    For indices and data they will be of size m, but for indices we don't
+    know the value yet so initialize it to the empty list. For data however, since
+    our edges are not valued, we know it will have only 1 as a value.
+    """
+    return (
+        np.zeros(number_of_nodes + 1),
+        [],
+        np.ones(number_of_edges, dtype=np.int8),
+    )
+
+
+def update_sparse_with_edge(
+    first_edge_node: int, second_edge_node: int, indptr: list, indices: list
+) -> None:
+    # First edge is noted i in comments and the second j, meaning the considered arc is (i,j)
+    # We know there is an indice more for row i to be stored, so increment in indptr the end position
+    # of row, meaning the value in position i + 1. If it was 0 it needs to become the value to the left
+    # + 1, else it is just incremented
+    if indptr[first_edge_node + 1] == 0:
+        indptr[first_edge_node + 1] = indptr[first_edge_node] + 1
+    else:
+        indptr[first_edge_node + 1] += 1
+    # We also need to put the right column position for the second edge in indices
+    indices.append(second_edge_node)
+
+    return
+
+
+def read_single_problem_from_file_as_sparse(
+    instance_file: TextIO,
+) -> Tuple[int, int, np.ndarray]:
+    """
+    Used to read a file containing a unique problem.
+
+    Stores the result as a scipy csr sparse array.
+    """
+    number_of_nodes, number_of_edges = read_instance_first_line(instance_file)
+    indptr, indices, data = initialize_sparse_array(number_of_nodes, number_of_edges)
+
+    for _ in range(number_of_edges):
+        first_edge_node, second_edge_node = read_instance_edge_line(instance_file)
+        update_sparse_with_edge(first_edge_node, second_edge_node, indptr, indices)
+
+    return (
+        number_of_nodes,
+        number_of_edges,
+        csr_array(
+            (data, np.array(indices), indptr), shape=(number_of_nodes, number_of_nodes)
+        ),
+    )
+
+
 def read_single_problem_from_path_as_sparse(
+    instance_path: str,
+) -> Tuple[int, int, csr_array]:
+    """
+    Used to read a file containing a unique problem from given path and
+    return the parameters, with the graph being represented as a scipy
+    sparse matrix.
+    """
+
+    with open(instance_path, "r") as instance_file:
+        return read_single_problem_from_file_as_sparse(instance_file)
+
+
+def read_single_problem_from_path_as_sparse_from_adjacency(
     instance_path: str,
 ) -> Tuple[int, int, csr_array]:
     """
@@ -79,46 +149,3 @@ def read_single_problem_from_path_as_sparse(
         adjacency_matrix,
     ) = read_single_problem_from_path_as_adjacency(instance_path)
     return number_of_nodes, number_of_edges, csr_array(adjacency_matrix)
-
-
-if __name__ == "__main__":
-    from constants import BASE_INSTANCES_DIR, BASE_INSTANCES_FILES
-
-    print(
-        read_single_problem_from_path_as_adjacency(
-            # instance_path=BASE_INSTANCES_DIR/ "brock200_2.col"
-            instance_path=BASE_INSTANCES_DIR
-            / "random-10.col"
-        )
-    )
-
-    """
-    from time import time
-
-    number_of_loads = 300
-
-    start_time = time()
-    for _ in range(number_of_loads):
-        read_single_problem_from_path_as_adjacency(
-            instance_path=BASE_INSTANCES_DIR / "brock200_2.col"
-        )
-    print(f"Mean time :{(time() - start_time)/number_of_loads}")
-    """
-
-    """
-    for file in BASE_INSTANCES_FILES:
-        _, _, array = read_single_problem_from_path_as_sparse(BASE_INSTANCES_DIR / file)
-        _, _, adjacency = read_single_problem_from_path_as_adjacency(
-            BASE_INSTANCES_DIR / file
-        )
-        from sys import getsizeof
-
-        print(
-            file,
-            getsizeof(array.indices)
-            + getsizeof(array.indptr)
-            + getsizeof(array.data)
-            + getsizeof(array),
-            getsizeof(adjacency),
-        )
-    """
