488454e13591bc0617b90d0fa197d8107c7ec218
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2022-10-16 15:25:35 +0200

    Tested heuristics on bigger graphs with known best

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ ROOT_DIR = Path.cwd()
 # Instances pathes
 INSTANCES_DIR = ROOT_DIR / "instances"
 BASE_INSTANCES_DIR = INSTANCES_DIR / "project_instances"
-OTHER_INSTANCES_DR = INSTANCES_DIR / "other_instances"
+OTHER_INSTANCES_DIR = INSTANCES_DIR / "other_instances"
 # Instances files groups
 BASE_INSTANCES_FILES = [
     "brock200_2.col",
@@ -21,5 +21,13 @@ OTHER_INSTANCES_FILES = [
     "C500.9.txt",
     "gen400_p0.9_75.txt",
     "p_hat1500-3.txt",
-    "p_hat700-1txt.txt",
+    "p_hat700-1.txt",
 ]
+OTHER_INSTANCES_BEST_KNOWN = {
+    "brock800_4.txt": 26,
+    "C500.9.txt": 57,
+    "C2000.5.txt": 16,
+    "gen400_p0.9_75.txt": 75,
+    "p_hat700-1.txt": 11,
+    "p_hat1500-3.txt": 94,
+}
