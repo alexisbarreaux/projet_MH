1e7227c603c7f153191e0f550bbd452a58221bbd
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2022-10-18 16:17:53 +0200

    Computed csv for other instances

--- meta_heuristics/base_vns.py
+++ meta_heuristics/base_vns.py
@@ -1,5 +1,6 @@
 # The idea is to start from a given clique and randomly remvoe nodes and attempt to rebuild
 # a clique from there.
+from typing import Tuple
 
 import numpy as np
 from time import time
@@ -14,7 +15,8 @@ def base_vns_meta_heuristic(
     degrees: np.ndarray,
     biggest_neighbourhood_size: int = 3,
     max_time: int = 60,
-) -> list:
+    verbose: bool = False,
+) -> Tuple[np.ndarray, float, int, int]:
     """
     Iteratively deletes random nodes from the clique and attempt to rebuild a better one from there.
 
@@ -24,12 +26,17 @@ def base_vns_meta_heuristic(
         - degrees : the degrees in our graph
         - biggest_neighbourhood_size: the max i for V_i our neighbourhood
         - max_time : max_time to run the heuristic in seconds.
+        - verbose : wether to print some informations during runtime or not.
     """
     # Setup
     best_clique = starting_clique
     size_best_clique = np.sum(best_clique)
     start_time = time()
+    time_best_found = 0
     number_of_iterations = 0
+    iteration_of_best = 0
+    # We can't take a neighbourhood bigger than all the nodes in the clique.
+    biggest_neighbourhood_size = min(np.sum(best_clique), biggest_neighbourhood_size)
 
     while (time() - start_time) < max_time:
         neighbourhood_size = 1
@@ -51,19 +58,23 @@ def base_vns_meta_heuristic(
             # See if the new result is better
             new_size = np.sum(new_clique)
             if new_size > size_best_clique:
-                print("Elapsed time", time() - start_time)
-                print(
-                    "Old",
-                    size_best_clique,
-                    "New",
-                    new_size,
-                    "Iteration",
-                    number_of_iterations,
-                )
+                time_best_found = time() - start_time
+                if verbose:
+                    print(
+                        "Elapsed time",
+                        time_best_found,
+                        "Old",
+                        size_best_clique,
+                        "New",
+                        new_size,
+                        "Iteration",
+                        number_of_iterations,
+                    )
                 size_best_clique = new_size
                 best_clique = new_clique
                 neighbourhood_size = 1
+                iteration_of_best = number_of_iterations
             else:
                 neighbourhood_size += 1
 
-    return best_clique
+    return best_clique, time_best_found, iteration_of_best, number_of_iterations
