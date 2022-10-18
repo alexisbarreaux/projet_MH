b0fd45a89615da3bec8a1cee349a4e1b188f1838
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2022-10-18 11:19:26 +0200

    Made sample neighbourhoods

--- utils/neighbourhood_utils.py
+++ utils/neighbourhood_utils.py
@@ -1,6 +1,7 @@
 import itertools
 
 import numpy as np
+import random
 
 
 def bit_flip(array: np.ndarray, positions: list[int]) -> np.ndarray:
@@ -14,6 +15,7 @@ def bit_flip(array: np.ndarray, positions: list[int]) -> np.ndarray:
 def i_th_bit_flip_neighbourhood(array: np.ndarray, i: int) -> list:
     """
     Creates the i-th neighbourhood of an array x, meaning V_i(x)
+    where the transformation is to flip a bit.
     """
     positions = np.arange(len(array))
     positions_combinations = itertools.combinations(positions, i)
@@ -21,3 +23,15 @@ def i_th_bit_flip_neighbourhood(array: np.ndarray, i: int) -> list:
         bit_flip(array, positions_combination)
         for positions_combination in positions_combinations
     ]
+
+
+def i_th_nodes_removal_neighbour(array: np.ndarray, i: int) -> list:
+    """
+    Creates a i-th neighbour of an array x, meaning x' in V_i(x).
+    Here the transformation is to remove at most i bits to 1 in the array,
+    meaning removing i-nodes.
+    The result returned is not the arrays but the separate combinations of
+    nodes one can remove.
+    """
+    clique_nodes = [i for i in range(len(array)) if array[i] == 1]
+    return random.sample(clique_nodes, i)
