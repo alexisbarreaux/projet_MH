ddf562b2ef8dc9448a951cf410d99c4c34b2bd14
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2022-10-25 11:54:10 +0200

    Added triangles in taboos meta

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,4 +4,5 @@ from .hyperparameters_utils import *
 from .instance_reading_utils import *
 from .neighbourhood_utils import *
 from .nodes_ordering_utils import *
+from .triangles_utils import *
 from .validity_checking_utils import *
--- utils/triangles_utils.py
+++ utils/triangles_utils.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 
 
@@ -21,3 +23,26 @@ def node_iterator(graph: np.ndarray):
                         in_triangle[neighbours[j]] = 1
                         in_triangle[u] = 1
     return in_triangle
+
+
+def build_subgraph_from_triangles(
+    graph: np.ndarray, degrees: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    triangle = node_iterator(graph)
+    sub_select = [i for i in range(len(graph)) if triangle[i]]
+
+    return triangle, graph[np.ix_(sub_select, sub_select)], np.take(degrees, sub_select)
+
+
+def build_clique_in_grah_from_subgraph(
+    triangle: np.ndarray, subgraph_clique: np.ndarray
+) -> np.ndarray:
+    size_of_whole_graph = len(triangle)
+    clique = np.zeros(size_of_whole_graph)
+    pos = 0
+    for node in range(size_of_whole_graph):
+        if triangle[node]:
+            if subgraph_clique[pos]:
+                clique[node] = 1
+            pos += 1
+    return clique
